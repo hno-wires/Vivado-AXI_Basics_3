f1d41bbe4da1bb75575fa5deb8e46f7bb3602a1d
Henrik Nordström <henrik.nordstrom@wires.se>
2025-02-20 16:33:58 +0100

    Import AXI_Basics_3 from https://adaptivesupport.amd.com/s/article/1058302

---
+++ create_proj.tcl
@@ -0,0 +1,110 @@
+###############################################################################
+ #  Copyright (c) 2018, Xilinx, Inc.
+ #  All rights reserved.
+ #
+ #  Redistribution and use in source and binary forms, with or without
+ #  modification, are permitted provided that the following conditions are met:
+ #
+ #  1.  Redistributions of source code must retain the above copyright notice,
+ #     this list of conditions and the following disclaimer.
+ #
+ #  2.  Redistributions in binary form must reproduce the above copyright
+ #      notice, this list of conditions and the following disclaimer in the
+ #      documentation and/or other materials provided with the distribution.
+ #
+ #  3.  Neither the name of the copyright holder nor the names of its
+ #      contributors may be used to endorse or promote products derived from
+ #      this software without specific prior written permission.
+ #
+ #  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ #  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ #  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ #  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ #  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ #  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ #  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ #  OR BUSINESS INTERRUPTION). HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ #  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ #  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ #  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ #
+###############################################################################
+ # @file create_proj.tcl
+ #
+ #	Tcl script for re-creating project AXI Basic Series 3
+ # 
+ #
+###############################################################################
+
+################################################################
+# Check if script is running in correct Vivado version.
+################################################################
+set scripts_vivado_version "2019.2"
+set current_vivado_version [version -short]
+
+# Uncomment the line below to generate the final project
+#set final 1
+ 
+if { [string first $current_vivado_version $scripts_vivado_version] == -1 } {
+   puts "The version $current_vivado_version is not supported. Supported versions are $scripts_vivado_version"
+   return 1
+}
+
+ if {[info exists final]} {
+	set file_path final
+} else {
+	set file_path src
+}
+
+# Configuration - Can be modified by the user
+set project_name 				AXI_Basics_3
+set BD_name 					AXI_GPIO_Sim
+
+# Create a new project
+create_project $project_name ./$project_name -part xc7z020clg484-1
+set_property target_language Verilog [current_project]
+
+# Create 'sources_1' fileset (if not found)
+if {[string equal [get_filesets -quiet sources_1] ""]} {
+  create_fileset -srcset sources_1
+}
+
+# Create 'constrs_1' fileset (if not found)
+if {[string equal [get_filesets -quiet constrs_1] ""]} {
+  create_fileset -constrset -quiet constrs_1
+}
+
+
+# Create 'sim_1' fileset (if not found)
+if {[string equal [get_filesets -quiet sim_1] ""]} {
+  create_fileset -simset sim_1
+}
+# Add simulation files
+add_files -fileset sim_1 -norecurse -scan_for_includes ./$file_path/sim
+import_files -fileset sim_1 -norecurse ./$file_path/sim
+
+set design_name 	$BD_name
+
+# Build the Block Design
+# Build the Block Design
+if { [string first $current_vivado_version "2019.2"] != -1 } {
+	source ./$file_path/tcl/bd.tcl
+}
+
+# Validate the BD
+regenerate_bd_layout
+validate_bd_design 
+save_bd_design
+
+#Generate the wrapper
+make_wrapper -files [get_files ${BD_name}.bd] -top
+
+# Add the wrapper to the fileset
+set obj [get_filesets sources_1]
+set files [list "[file normalize [glob "./$project_name/$project_name.srcs/sources_1/bd/$BD_name/hdl/${BD_name}_wrapper.v"]]"]
+add_files -norecurse -fileset $obj $files
+
+# Generate the output products
+#generate_target all [get_files ./$project_name/$project_name.srcs/sources_1/bd/$BD_name/$BD_name.bd]
+#create_ip_run [get_files -of_objects [get_fileset sources_1] ./$project_name/$project_name.srcs/sources_1/bd/$BD_name/$BD_name.bd]
+#launch_runs -jobs 8 [get_runs $BD_name*synth_1]
---
+++ sim/AXI_GPIO_tb.sv
@@ -0,0 +1,149 @@
+`timescale 1ns / 1ps
+//-----------------------------------------------------------------
+// (c) Copyright 1984 - 2018 Xilinx, Inc. All rights reserved.	
+//							
+// This file contains confidential and proprietary information
+// of Xilinx, Inc. and is protected under U.S. and
+// international copyright and other intellectual property
+// laws.							
+//-----------------------------------------------------------------							 
+// DISCLAIMER							
+// This disclaimer is not a license and does not grant any	 
+// rights to the materials distributed herewith. Except as	 
+// otherwise provided in a valid license issued to you by	
+// Xilinx, and to the maximum extent permitted by applicable
+// law: (1) THESE MATERIALS ARE MADE AVAILABLE "AS IS" AND
+// WITH ALL FAULTS, AND XILINX HEREBY DISCLAIMS ALL WARRANTIES
+// AND CONDITIONS, EXPRESS, IMPLIED, OR STATUTORY, INCLUDING
+// BUT NOT LIMITED TO WARRANTIES OF MERCHANTABILITY, NON-	 
+// INFRINGEMENT, OR FITNESS FOR ANY PARTICULAR PURPOSE; and	 
+// (2) Xilinx shall not be liable (whether in contract or tort,	
+// including negligence, or under any other theory of
+// liability) for any loss or damage of any kind or nature 
+// related to, arising under or in connection with these	 
+// materials, including for any direct, or any indirect,	
+// special, incidental, or consequential loss or damage		 
+// (including loss of data, profits, goodwill, or any type of	
+// loss or damage suffered as a result of any action brought	
+// by a third party) even if such damage or loss was		 
+// reasonably foreseeable or Xilinx had been advised of the	 
+// possibility of the same.					 
+//								 
+// CRITICAL APPLICATIONS					 
+// Xilinx products are not designed or intended to be fail-	 
+// safe, or for use in any application requiring fail-safe	 
+// performance, such as life-support or safety devices or	 
+// systems, Class III medical devices, nuclear facilities,	 
+// applications related to the deployment of airbags, or any	 
+// other applications that could lead to death, personal	 
+// injury, or severe property or environmental damage		 
+// (individually and collectively, "Critical			 
+// Applications"). Customer assumes the sole risk and		 
+// liability of any use of Xilinx products in Critical		 
+// Applications, subject only to applicable laws and		 
+// regulations governing limitations on product liability.	 
+//								 
+// THIS COPYRIGHT NOTICE AND DISCLAIMER MUST BE RETAINED AS	 
+// PART OF THIS FILE AT ALL TIMES. 				 
+//-----------------------------------------------------------------
+// ************************************************************************
+//
+//-----------------------------------------------------------------------------
+// Filename:        AXI_GPIO_tb.sv
+// Version:         v1.0
+// Description:     Simulation test bench for the AXI Basics Series 3
+//                  
+//-----------------------------------------------------------------------------
+//Step 2 - Import two required packages: axi_vip_pkg and <component_name>_pkg.
+
+
+//////////////////////////////////////////////////////////////////////////////////
+// Test Bench Signals
+//////////////////////////////////////////////////////////////////////////////////
+// Clock and Reset
+bit aclk = 0, aresetn = 1;
+//Simulation output
+bit led_1, switch_1;
+//AXI4-Lite signals
+xil_axi_resp_t 	resp;
+bit[31:0]  addr, data, base_addr = 32'h4400_0000, switch_state;
+
+module AXI_GPIO_tb( );
+
+AXI_GPIO_Sim_wrapper UUT
+(
+    .aclk               (aclk),
+    .aresetn            (aresetn),
+    .led_1              (led_1),
+    .switch_1           (switch_1)
+);
+
+// Generate the clock : 50 MHz    
+always #10ns aclk = ~aclk;
+
+//////////////////////////////////////////////////////////////////////////////////
+// Main Process
+//////////////////////////////////////////////////////////////////////////////////
+//
+initial begin
+    //Assert the reset
+    aresetn = 0;
+    #340ns
+    // Release the reset
+    aresetn = 1;
+end
+//
+//////////////////////////////////////////////////////////////////////////////////
+// The following part controls the AXI VIP. 
+//It follows the "Usefull Coding Guidelines and Examples" section from PG267
+//////////////////////////////////////////////////////////////////////////////////
+//
+// Step 3 - Declare the agent for the master VIP
+
+//
+initial begin    
+
+    // Step 4 - Create a new agent
+    
+    
+    // Step 5 - Start the agent
+    
+    
+    //Wait for the reset to be released
+    wait (aresetn == 1'b1);
+	#200ns
+	
+    
+    //Send 0x1 to the AXI GPIO Data register 1
+    
+    
+    
+    
+    
+    //Send 0x0 to the AXI GPIO Data register 1
+	
+    // Switch in OFF position
+    switch_1 = 0;
+    // Read the AXI GPIO Data register 2
+	
+    // Switch in ON position
+	switch_1 = 1;
+	// Read the AXI GPIO Data register 2
+	
+
+end
+//
+//////////////////////////////////////////////////////////////////////////////////
+// Simulation output processes
+//////////////////////////////////////////////////////////////////////////////////
+//
+always @(posedge led_1)
+begin
+     $display("led 1 ON");
+end
+
+always @(negedge led_1)
+begin
+     $display("led 1 OFF");
+end
+endmodule
